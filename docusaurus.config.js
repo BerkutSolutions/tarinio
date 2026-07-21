3ae3faf155afbff0113b657d337fba2725e9a8da
berkutsolutions <keliberda99@yandex.ru>
2026-07-21 15:07:28 +0300

    release: prepare 1.5.7 publication evidence

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -97,10 +97,10 @@ const config = {
           ],
         },
         {
-          label: 'Version 1.5.6',
+          label: 'Version 1.5.7',
           position: 'right',
           items: [
-            {label: 'Current Release 1.5.6', to: '/ru/core-docs/release-policy/'},
+            {label: 'Current Release 1.5.7', to: '/ru/core-docs/release-policy/'},
             {label: 'CHANGELOG', href: 'https://github.com/BerkutSolutions/tarinio/blob/main/CHANGELOG.md'},
           ],
         },
